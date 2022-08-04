882125fc388bdec282df80790143c8c934ad1c24
VladimirVlasov1982 <vladimir.vlasov82@mail.ru>
2022-08-04 16:26:32 +0300

    add files

---
+++ models.py
@@ -0,0 +1,57 @@
+# Модели и их схемы
+from config import db
+from marshmallow import Schema, fields
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+movie_shema = MovieSchema()
+movies_shema = MovieSchema(many=True)
+
+director_shema = DirectorSchema()
+directors_shema = DirectorSchema(many=True)
+
+genre_shema = GenreSchema()
+genres_shema = GenreSchema(many=True)
